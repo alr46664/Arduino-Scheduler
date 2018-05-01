48478c5f7070a080e97247fbf9483d436b5641c7
Andre Madureira <andreluizromano@hotmail.com>
2018-04-30 21:56:02 -0300

    initial

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,1 +1,27 @@
+
+#include "Scheduler.h"
+
+// ------------
+//   PUBLIC
+// ------------
+
+Scheduler::Scheduler(unsigned long timeout) : timeout(timeout) { }
+
+void Scheduler::start(){
+    started = true;
+    prev_time = millis();
+}
+
+void Scheduler::stop(){
+    started = false;
+}
+
+bool Scheduler::hasTimeoutHappened(){
+    return started && ((unsigned long)(millis() - prev_time) >= timeout);
+}
+
+
+// ------------
+//   PRIVATE
+// ------------
  
--- src/Scheduler.h
+++ src/Scheduler.h
@@ -1,1 +1,21 @@
- 
+#ifndef _Scheduler_H
+#define _Scheduler_H
+
+#include "Arduino.h" 
+
+class Scheduler {
+public:
+    Scheduler(unsigned long timeout);
+    
+    void start();
+    void stop();
+    bool hasTimeoutHappened();
+
+private:
+    bool started;
+    unsigned long prev_time;
+    unsigned long timeout;
+
+};
+
+#endif  // _Scheduler_H 
